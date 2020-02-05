b84f852b126881247dbd101bff5f1ffb70f9b4bf
Ruby Rinken <rubyrinken@gmail.com>
2020-02-05 15:32:44 -0700

    Refractored timer code, cleaned up HTML a bit

--- main.js
+++ main.js
@@ -1,46 +1,45 @@
+var globalTimer = null;
 
+function formatTimeString(seconds) {
+  var sec_num = Number(seconds); 
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-function timer(){
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  if (minutes < 10 && hours > 0) { 
+    minutes = "0" + minutes; 
+  }
+
+  if (hours > 0)
+    return hours + ':' + minutes + ':' + seconds;
+  else if (seconds >= 0)
+    return minutes + ':' + seconds;
+  else 
+    return
+}
+
+function startTimer() {
+  if (globalTimer === null) {
     var min = document.getElementById('minIn').value;
     var sec = document.getElementById('secIn').value;
-    var seconds = Number(min*60) + Number(sec);
-    var timer = setInterval(function(){
-        var hourForm = (seconds - seconds%3600)/3600;
-        var minForm = (seconds - hourForm*3600 - seconds%60)/60;
-        var secForm = seconds%60;
-        //---------Logic to format hour, minute, and second to 00:00:00--------
-        if (hourForm == 0 && minForm >= 1 && minForm < 10 && secForm < 10){
-          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:0${secForm.toFixed(0)}`; //M:0S
-        } else if (hourForm == 0 && minForm == 0 && secForm >= 10){
-          document.getElementById('done').innerHTML = `:${secForm.toFixed(0)}`; //:SS
-        } else if (hourForm == 0 && minForm == 0 && secForm < 10){
-          document.getElementById('done').innerHTML = `:0${secForm.toFixed(0)}`; //:0S
-        } else if (hourForm == 0 && minForm < 10 && secForm >= 10){
-          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:${secForm.toFixed(0)}`; //M:SS
-        } else if (hourForm == 0 && minForm >= 10 && secForm < 10){
-          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//MM:0S
-        } else if (hourForm == 0 && minForm >= 10 && secForm >= 10){
-          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//MM:SS
-        } else if (hourForm >= 10 && minForm < 10 && secForm < 10){
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//HH:0M:0S
-        } else if (hourForm >= 10 && minForm >= 10 && secForm < 10){
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//HH:MM:0S
-        } else if (hourForm > 0 && hourForm < 10 && minForm >= 10 && secForm >= 10){
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//H:MM:SS
-        } else if (hourForm > 0 && hourForm < 10 && minForm < 10 && secForm >= 10){
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//H:0M:SS
-        } else if (hourForm > 0 && hourForm < 10 && minForm < 10 && secForm < 10){
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//H:0M:0S
-        } else if (hourForm > 0 && hourForm < 10 && minForm > 10 && secForm < 10){
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//H:MM:0S
-        } else {
-          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//HH:MM:SS
-        }
-        //---------------------------------------------------------------------
-        seconds--;
-        if (seconds < 0) {
-            clearInterval(timer);
-            document.getElementById('done').innerHTML= `Time is up! You waited ${min} minutes and ${sec} seconds.`;
-        }
-    }, 1000);
+    timer(min, sec);
+  }
+}
+
+function timer(min, sec){
+  var totalTime = Number(min*60) + Number(sec);
+  document.getElementById('done').innerHTML = formatTimeString(totalTime);
+  globalTimer = setInterval(function(){
+    totalTime--;
+    if (totalTime < 0) {
+        clearInterval(globalTimer);
+        document.getElementById('done').innerHTML= `Time is up! You waited ${min + ((min) ? "minutes and " : "")}${sec} seconds.`;
+        globalTimer = null;
+    } else {
+      document.getElementById('done').innerHTML = formatTimeString(totalTime);
+    }
+  }, 1000); 
 }
