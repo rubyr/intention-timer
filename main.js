c9f6b3dabfdac235f97a5ada3161d6154a61d0fa
Kyle Williams <kylecharleswilliams@gmail.com>
2020-02-04 19:55:13 -0700

    Add logic to format hour, minute, and seconds

--- main.js
+++ main.js
@@ -5,9 +5,29 @@ function timer(){
     var sec = document.getElementById('secIn').value;
     var seconds = Number(min*60) + Number(sec);
     var timer = setInterval(function(){
-        var minForm = (seconds - seconds%60)/60;
-        var secForm = Number(seconds%60);
-        document.getElementById('done').innerHTML= `You have ${minForm} minutes and ${secForm} seconds left!!!`;
+        var hourForm = (seconds - seconds%3600)/3600;
+        var minForm = (seconds - hourForm*3600 - seconds%60)/60;
+        var secForm = seconds%60;
+        //Logic to format hour, minute, and second to 00:00:00
+        if (hourForm == 0 && minForm < 10 && secForm < 10){
+          document.getElementById('done').innerHTML= `0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;
+        } else if (hourForm == 0 && minForm < 10 && secForm >= 10){
+          document.getElementById('done').innerHTML= `0${minForm.toFixed(0)}:${secForm.toFixed(0)}`;
+        } else if (hourForm == 0 && minForm >= 10 && secForm < 10){
+          document.getElementById('done').innerHTML= `${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;
+        } else if (hourForm == 0 && minForm >= 10 && secForm >= 10){
+          document.getElementById('done').innerHTML= `${minForm.toFixed(0)}:${secForm.toFixed(0)}`;
+        } else if (hourForm >= 10 && minForm < 10 && secForm < 10){
+          document.getElementById('done').innerHTML= `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;
+        } else if (hourForm >= 10 && minForm >= 10 && secForm < 10){
+          document.getElementById('done').innerHTML= `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;
+        } else if (hourForm > 0 && hourForm < 10 && minForm >= 10 && secForm >= 10){
+          document.getElementById('done').innerHTML= `0${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;
+        } else if (hourForm > 0 && hourForm < 10 && minForm < 10 && secForm >= 10){
+          document.getElementById('done').innerHTML= `0${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:${secForm.toFixed(0)}`;
+        } else {
+          document.getElementById('done').innerHTML= `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;
+        }
         seconds--;
         if (seconds < 0) {
             clearInterval(timer);
