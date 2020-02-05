81e8abb0d03d04b431d395db5fe97e771aa2690a
Kyle Williams <kylecharleswilliams@gmail.com>
2020-02-04 20:11:30 -0700

    Add logic for timer corner case

--- main.js
+++ main.js
@@ -10,23 +10,25 @@ function timer(){
         var secForm = seconds%60;
         //---------Logic to format hour, minute, and second to 00:00:00--------
         if (hourForm == 0 && minForm < 10 && secForm < 10){
-          document.getElementById('done').innerHTML= `0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`; //0M:0S
+          document.getElementById('done').innerHTML = `0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`; //0M:0S
         } else if (hourForm == 0 && minForm < 10 && secForm >= 10){
-          document.getElementById('done').innerHTML= `0${minForm.toFixed(0)}:${secForm.toFixed(0)}`; //0M:SS
+          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:${secForm.toFixed(0)}`; //M:SS
         } else if (hourForm == 0 && minForm >= 10 && secForm < 10){
-          document.getElementById('done').innerHTML= `${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//MM:0S
+          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//MM:0S
         } else if (hourForm == 0 && minForm >= 10 && secForm >= 10){
-          document.getElementById('done').innerHTML= `${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//MM:SS
+          document.getElementById('done').innerHTML = `${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//MM:SS
         } else if (hourForm >= 10 && minForm < 10 && secForm < 10){
-          document.getElementById('done').innerHTML= `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//HH:0M:0S
+          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//HH:0M:0S
         } else if (hourForm >= 10 && minForm >= 10 && secForm < 10){
-          document.getElementById('done').innerHTML= `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//HH:MM:0S
+          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//HH:MM:0S
         } else if (hourForm > 0 && hourForm < 10 && minForm >= 10 && secForm >= 10){
-          document.getElementById('done').innerHTML= `0${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//0H:MM:SS
+          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//H:MM:SS
         } else if (hourForm > 0 && hourForm < 10 && minForm < 10 && secForm >= 10){
-          document.getElementById('done').innerHTML= `0${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//0H:0M:SS
-        } else {
-          document.getElementById('done').innerHTML= `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//HH:MM:SS
+          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//H:0M:SS
+        } else if (hourForm > 0 && hourForm < 10 && minForm < 10 && secForm < 10){
+          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:0${minForm.toFixed(0)}:0${secForm.toFixed(0)}`;//H:0M:0S
+        }else {
+          document.getElementById('done').innerHTML = `${hourForm.toFixed(0)}:${minForm.toFixed(0)}:${secForm.toFixed(0)}`;//HH:MM:SS
         }
         //---------------------------------------------------------------------
         seconds--;
